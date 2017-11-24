f9d53cbd046609e0ce6750540e1802faf030190e
hulianghai <hulianghai@xmzhen.com>
2017-11-24 17:49:42 +0800

    * 简单描述一下干了什么 What: fix
    * 为什么要这么做
    Why:
    
    * 怎样做的？有何副作用？
    How:☺

--- control/start.sh
+++ control/start.sh
@@ -3,7 +3,7 @@
 uwsgiini="`pwd`/uwsgi.ini"
 touchfile="`pwd`/control/touch_reload_uwsgi"
 touchlogfile="`pwd`/control/touch_reopen_log"
-UWSGI_PATH="/home/panda/workplace/env/bin/uwsgi"
+UWSGI_PATH="/root/workspace/blogenv/bin/uwsgi"
 
-${UWSGI_PATH} --uid panda -x ${uwsgiini} --touch-reload=${touchfile} --touch-logreopen=${touchlogfile} --req-logger=file:/var/log/uwsgi/uwsgi_req.log --logger=file:/var/log/uwsgi/uwsgi_error.log
+${UWSGI_PATH} --uid root --ini ${uwsgiini} --touch-reload=${touchfile} --touch-logreopen=${touchlogfile} --req-logger=file:/var/log/uwsgi/uwsgi_req.log --logger=file:/var/log/uwsgi/uwsgi_error.log
 

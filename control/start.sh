a1a50780cad1ba2b98c461feee12f11990fcd9fe
hulianghai <hulianghai@xmzhen.com>
2017-11-27 11:16:57 +0800

    * 简单描述一下干了什么 What: fix
    * 为什么要这么做
    Why:
    
    * 怎样做的？有何副作用？
    How:☺

--- control/start.sh
+++ control/start.sh
@@ -5,5 +5,5 @@ touchfile="`pwd`/control/touch_reload_uwsgi"
 touchlogfile="`pwd`/control/touch_reopen_log"
 UWSGI_PATH="/root/workspace/env/bin/uwsgi"
 
-${UWSGI_PATH} --uid root --ini ${uwsgiini} --touch-reload=${touchfile} --touch-logreopen=${touchlogfile} --req-logger=file:/var/log/uwsgi/uwsgi_req.log --logger=file:/var/log/uwsgi/uwsgi_error.log
+${UWSGI_PATH} --uid root --ini ${uwsgiini} --touch-reload=${touchfile} --touch-logreopen=${touchlogfile} --req-logger=file:/root/workspace/DBlog/log/uwsgi_req.log --logger=file:/root/workspace/DBlog/log/uwsgi_error.log
 

3ce35ee9fc02c3298f49d6f1e1cca610db8810d2
hulianghai <hulianghai@xmzhen.com>
2017-11-27 16:14:14 +0800

    * 简单描述一下干了什么 What: fix
    * 为什么要这么做
    Why:
    
    * 怎样做的？有何副作用？
    How:☺

--- settings.py
+++ settings.py
@@ -123,6 +123,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGS_BASE_DIR = os.path.join(BASE_DIR, 'log')
 LOGGING = {

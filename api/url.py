9f20b377f2cdaa42a678a670187323ab5db1056a
hulianghai <hulianghai@xmzhen.com>
2017-11-27 17:08:34 +0800

    * 简单描述一下干了什么 What: fix
    * 为什么要这么做
    Why:
    
    * 怎样做的？有何副作用？
    How:☺

--- api/url.py
+++ api/url.py
@@ -6,6 +6,8 @@ from api.views import *
 urlpatterns = [
     url(r'^$', home_view),
     url(r'^tag/(?P<id>\d+)/$', blog_filer_view, name='filterblog'),
-    # 博客 添加，删除，修改
+    # 博客 添加，删除，修改，显示
     url(r'^add/$', blog_add_view, 'addblog'),
+    url(r'^show/(?P<id>\d+)/$', blog_show_view, 'detailblog'),
+    url(r'^update/(?P<id>\d+)/$', blog_update_view, 'updateblog'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import HttpResponseRedirect
+from __builtin__ import unicode
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import HttpResponse, render_to_response, render
 
 # Create your views here.
@@ -42,9 +43,73 @@ def blog_add_view(request):
                 blog.tags.add(Tag.objects.get(tag_name=taglist.strip()))
                 blog.save()
             id = Blog.objects.order_by('-publish_time')[0].id
-            return HttpResponseRedirect('/sblog/blog/%s' % id)
+            return HttpResponseRedirect('/blog/show/%s' % id)
     else:
         form = BlogForm()
         tag = TagForm(initial={'tag_name': 'notags'})
     return render_to_response('blog_add.html',
                               {'form': form, 'tag': tag}, context_instance=RequestContext(request))
+
+
+def blog_show_view(request, id):
+    try:
+        blog = Blog.objects.get(id=id)
+        tags = Tag.objects.all()
+    except Blog.DoesNotExist:
+        raise Http404
+    return render_to_response("blog_show.html",
+                              {"blog": blog, "tags": tags},
+                              context_instance=RequestContext(request))
+
+
+def blog_update_view(request, id):
+    id = id
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        tag = TagForm(request.POST)
+        if form.is_valid() and tag.is_valid():
+            cd = form.cleaned_data
+            cdtag = tag.cleaned_data
+            tagname = cdtag['tag_name']
+            tagnamelist = tagname.split()
+            for taglist in tagnamelist:
+                Tag.objects.get_or_create(tag_name=taglist.strip())
+            title = cd['caption']
+            content = cd['content']
+            blog = Blog.objects.get(id=id)
+            if blog:
+                blog.caption = title
+                blog.content = content
+                blog.save()
+                for taglist in tagnamelist:
+                    blog.tags.add(Tag.objects.get(tag_name=taglist.strip()))
+                    blog.save()
+                tags = blog.tags.all()
+                for tagname in tags:
+                    tagname = unicode(str(tagname), "utf-8")
+                    if tagname not in tagnamelist:
+                        notag = blog.tags.get(tag_name=tagname)
+                        blog.tags.remove(notag)
+            else:
+                blog = Blog(caption=blog.caption, content=blog.content)
+                blog.save()
+            return HttpResponseRedirect('/sblog/blog/%s' % id)
+    else:
+        try:
+            blog = Blog.objects.get(id=id)
+        except Exception:
+            raise Http404
+        form = BlogForm(initial={'caption': blog.caption, 'content': blog.content}, auto_id=False)
+        tags = blog.tags.all()
+        if tags:
+            taginit = ''
+            for x in tags:
+                taginit += str(x) + ' '
+            tag = TagForm(initial={'tag_name': taginit})
+        else:
+            tag = TagForm()
+    return render_to_response('blog_add.html',
+                              {'blog': blog, 'form': form, 'id': id, 'tag': tag},
+                              context_instance=RequestContext(request))
+
+
